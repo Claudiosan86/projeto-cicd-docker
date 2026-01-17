231ad619c6f208a8b61588f1f0ae170b78e0d677
Claudio San Roman Rangel <claudio.sanroman23@gmail.com>
2026-01-17 15:59:49 -0300

    fix: adjust flask bind to 0.0.0.0 for docker networking

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
-print("###Pipeline CI/CD Claudio San Roman ###")
-print("Status: Container Docker rodando com sucesso!")
+from flask import Flask
+app = Flask(__name__)
 
+@app.route('/')
+def hello():
+    return "Projeto CI/CD do Claudio San Roman - N3 com Sucesso!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
